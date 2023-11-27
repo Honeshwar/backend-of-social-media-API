f222c865acf668790551314be4b88a203ee19241
CodeSandbox <support@codesandbox.io>
2023-11-27 09:19:49 +0000

    Sandbox snapshot commit

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,10 @@
 const route = require("express").Router();
+const userController = require("../controllers/usersControllers");
 
-route.use("/api/users",(req,res)=>{})
-route.use("/api/users",(req,res)=>{})
+route.put("/:id",userController.update) //update user
+route.get("/:id",userController.get)//get a user, we never get all user in app
+route.delete("/:id",userController.delete) //delete user
+route.put("/:id/follow",userController.follow) //follow user
+route.put("/:id/unfollow",userController.unFollow) //unfollow user
+
 module.exports = route;
